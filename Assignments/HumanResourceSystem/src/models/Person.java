52dc255a6715d6933224ce34c0f66503f1503611
Anuj Kumar <anujkumar@Anujs-MacBook-Pro.local>
2022-09-27 18:33:48 -0400

    Added UI elements

---
+++ Assignments/HumanResourceSystem/src/models/Person.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author anujkumar
+ */
+public class Person {
+    private String name;
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+    
+}
